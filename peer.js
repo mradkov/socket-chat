e3ed2cda80e0941182182cff8da7c21f6838e27d
Milen Radkov <milenradkov@me.com>
2019-02-11 16:23:52 +0200

    message sequence

--- peer.js
+++ peer.js
@@ -9,27 +9,38 @@ var peers = process.argv.slice(3)
 
 var swarm = topology(toAddress(me), peers.map(toAddress))
 var streams = streamSet() 
+var id = Math.random()
+var seq = 0
+var logs = {}
 
 register(me)
 
 swarm.on('connection', function (peer) {
-        console.log('[a peer joined]')
-        peer = jsonStream(peer)
-        streams.add(peer)
-        peer.on('data', function (data) {
-                console.log(data.username + '> ' + data.message)
-        })
+  console.log('[a peer joined]')
+  peer = jsonStream(peer)
+  streams.add(peer)
+  peer.on('data', function (data) {
+    if (logs[data.log] <= data.seq) return
+    logs[data.log] = data.seq
+    console.log(data.username + '> ' + data.message)
+    streams.forEach(function (otherPeers) {
+      otherPeers.write(data)
+    })
+  })
 })
 
 process.stdin.on('data', function(data) {
-        streams.forEach(function (peer) {
-                peer.write({
-                        username: me,
-                        message: data.toString().trim()
-                })
-        })
+  var next = seq++
+  streams.forEach(function (peer) {
+    peer.write({
+      log: id,
+      seq: next,
+      username: me,
+      message: data.toString().trim()
+    })
+  })
 })
 
 function toAddress (name) {
-        return name + '.local:' + toPort(name)
+  return name + '.local:' + toPort(name)
 }
